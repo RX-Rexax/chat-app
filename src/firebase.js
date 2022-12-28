59f407287322b3ce7084f2430355fbc7c8dde4ba
RX-Rexax <80836157+RX-Rexax@users.noreply.github.com>
2022-12-28 17:58:40 +0800

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQndPv7xvwoQXwv6hvd_JRcIQxVavFDm4",
+  authDomain: "chat-app-90944.firebaseapp.com",
+  projectId: "chat-app-90944",
+  storageBucket: "chat-app-90944.appspot.com",
+  messagingSenderId: "189805812421",
+  appId: "1:189805812421:web:2e5baa5997f6613876eeac",
+  measurementId: "G-5E9KEYV18D"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
